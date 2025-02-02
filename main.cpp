05275698d73a33c81a4a1d198daafd053843dd25
Christian <graetz23@gmail.com>
2025-02-02 20:22:54 +0100

    This is the way: Mando says ..

--- main.cpp
+++ main.cpp
@@ -32,45 +32,47 @@ using namespace PAXCC;
 int main(int arc, char **argv)
 {
 
-  PaxMap<std::string> map; // generally used hashmap ..
-  map.dbg();               // debugging the pax map ..
+  Pax *pax1 = new Pax("Bob", "Dylon");
+  pax1->Attrib()->add("plays", "guitar");
+  pax1->Attrib()->add("sings", "songs");
 
-  Pax *pax1 = new Pax();
-  pax1->Tag("Bob");
-  pax1->Val("Dylon");
-  std::cout << pax1->Tag() << " => " << pax1->Val() << std::endl
+  Pax *pax2 = new Pax("Dolly", "Parton");
+  pax2->Attrib()->add("sings", "songs");
+  pax2->Attrib()->add("plays", "country guitar");
+
+  Pax *pax3 = new Pax("Johnny", "Cash");
+  pax3->Attrib()->add("plays", "guitar");
+  pax3->Attrib()->add("sings", "country songs");
+
+  Pax *pax4 = new Pax("John", "Denver");
+  pax4->Attrib()->add("sings", "country songs");
+  pax4->Attrib()->add("plays", "country guitar");
+
+  pax1->Child()->add(pax2); // pax2 as child of pax1
+  pax2->Child()->add(pax3); // pax3 as child of pax2
+  pax2->Child()->add(pax4); // pax3 as child of pax2
+
+  std::string xml = pax1->XML();
+  std::cout << xml << std::endl
             << std::flush;
+  
+  Pax* pax2_ = pax1->Child("Dolly");
 
-  Pax *pax2 = new Pax();
-  pax2->Tag("Dolly");
-  pax2->Val("Parton");
-  pax2->Attrib()->add("sings", "country songs");
-  pax1->Child()->add(pax2);
+  Pax* pax3_ = pax2->Child("Johnny"); // from pax2
+  
+  Pax* pax4_ = pax1->Child("Dolly")->Child("John"); // chained
+  
+  std::string val3_ = pax1->Child("Dolly")->Child("Johnny")->Val();
 
-  Pax *pax2_ = new Pax();
-  pax2_->Tag("Dolly");
-  pax2_->Val("Parton");
-  pax2_->Attrib()->add("sings", "country songs");
-  pax1->Child()->add(pax2_);
+  Pax* pax = pax1->Child("Dolly")->Child("John"); // chained
+  std::string tag = pax->Tag();
+  std::string val = pax->Val();
+  std::string attribTag = pax->Attrib("sings")->Tag();
+  std::string attribVal = pax->Attrib("sings")->Val();
 
 
-  Pax *pax3 = new Pax();
-  pax3->Tag("Johnny");
-  pax3->Val("Cash");
-
-  pax2->Child()->add(pax3);
-
-  std::string val2_ = pax1->Child()->get("Dolly")->Val();
-  Pax *pax3_ = pax2->Child()->get("Johnny");
-  std::string val3_ = pax1->Child()->get("Dolly")->Child()->get("Johnny")->Val();
-
-  Pax *pax3__ = pax2->Child("Johnny");
-  pax3__->Attrib()->add("plays", "guitar");
-  pax3__->Attrib()->add("sings", "country songs");
-  std::string val3__ = pax1->Child("Dolly")->Child("Johnny")->Val();
-
-  std::string xml = pax1->XML();  // generate XML from object tree ..
-  std::cout << xml << std::flush; // print out
+  std::string xml_ = pax1->XML();  // generate XML from object tree ..
+  std::cout << xml_ << std::flush; // print out
 
   delete pax1; // working recursively well on first run ..
 
